6bd1d9bd7957d17a268026391a36680a12bb332b
sudonotpseudo <sudonotpseudo@gmail.com>
2016-11-12 19:45:55 +0000

    crawler webscrpit

---
+++ crawlerInfo.php
@@ -0,0 +1,23 @@
+
+<?php
+//Written By Ben Weinfeld
+//Bar Crawler Info Script
+function barInfo() {
+	echo "data": {"info": "This is info"};
+}
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allows-Methods: GET, POST');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Content-Type Authorization, X-Requested-With');
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+	getNextOrder();;
+}
+elseif($_SERVER["REQUEST_METHOD"] == "GET"){
+	getNextOrder();
+}
+
+else{
+	exit(0);
+}
+?>
