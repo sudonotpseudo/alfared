d4fea2354717ba67ccfc3e2133e5f6ed45ed4e74
sabrinasmai <sabrina.smai@mail.utoronto.ca>
2016-11-13 07:31:18 -0500

    Add files via upload

---
+++ button_code.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $("#soybutton").click(function(){
+    $("div").css("background-color","yellow")
+    $("bullbutton").click(function(){
+      $("div").css("background-color","yellow")
+      $("surbutton").click(function(){
+        $("div").css("background-color","yellow")
+});
+});
+});
